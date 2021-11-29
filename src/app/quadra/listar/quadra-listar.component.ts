b670e12b835a70c2508dac9c7d5f5440af109584
marcosmatos1203 <marcoscaiobel@gmail.com>
2021-11-29 16:41:08 -0300

    quadra listar pronto

--- src/app/quadra/listar/quadra-listar.component.ts
+++ src/app/quadra/listar/quadra-listar.component.ts
@@ -4,7 +4,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IHttpPomarService } from 'src/app/shared/interfaces/IHttpPomarService';
 import { IHttpQuadraService } from 'src/app/shared/interfaces/IHttpQuadraService';
 import { ToastService } from 'src/app/shared/services/toast.service';
-import { PomarEditViewModel } from 'src/app/shared/viewModels/Pomar/PomarEditViewModel';
+import { PomarListViewModel } from 'src/app/shared/viewModels/Pomar/PomarListViewModel';
+import { QuadraListViewModel } from 'src/app/shared/viewModels/Quadra/QuadraListViewModel';
 
 @Component({
   selector: 'app-quadra-listar',
@@ -13,15 +14,45 @@ import { PomarEditViewModel } from 'src/app/shared/viewModels/Pomar/PomarEditVie
 })
 export class QuadraListarComponent implements OnInit {
 
-  pomar:PomarEditViewModel;
+  listaQuadrasTotal:QuadraListViewModel[];
+  listaQuadras:QuadraListViewModel[];
+  pomar:PomarListViewModel;
   quadraSelecionado: any;
   id:any;
+
+  page = 1;
+  pageSize = 5;
+  collectionSize = 0;
+
   constructor(private servicoModal: NgbModal, 
     @Inject('IHttpQuadraServiceToken') private servicoQuadra: IHttpQuadraService,
     @Inject('IHttpPomarServiceToken') private servicoPomar: IHttpPomarService,
   private router: Router,private _Activatedroute: ActivatedRoute, private toastService: ToastService) { }
 
   ngOnInit(): void {
+    this.id = this._Activatedroute.snapshot.paramMap.get("id");
+    this.obterPomar(this.id);
+    this.obterQuadras(this.id);
+  }
+  obterPomar(id:any){
+    this.servicoPomar.obterPorId(id)
+    .subscribe(resposta => {
+      this.pomar = resposta;       
+    });
+  }
+  obterQuadras(id:any){
+    this.servicoQuadra.obterQuadraPorIdPomar(id)
+    .subscribe(resposta => {
+      this.listaQuadrasTotal = resposta;
+      this.atualizar();
+    });
+  }
+  atualizar() {
+    this.listaQuadras = this.listaQuadrasTotal
+      .map((obj, i) => ({ u: i + 1, ...obj }))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+
+    this.collectionSize = this.listaQuadrasTotal.length;
   }
   abrirConfirmacao(modal: any) {
     this.servicoModal.open(modal).result.then((resultado) => {
--- src/app/shared/viewModels/Quadra/QuadraListViewModel.ts
+++ src/app/shared/viewModels/Quadra/QuadraListViewModel.ts
@@ -4,5 +4,6 @@ import { PomarListViewModel } from "../Pomar/PomarListViewModel";
 export class QuadraListViewModel{
     id: number;
     anoPlantio: number;
+    quantidadeColmeias: number;
     pomar: PomarListViewModel;
 }
